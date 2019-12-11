34df60a461c58c5fde1e88cd89b610ec595f757f
Tarangok <tarangok@mail.ru>
2019-12-12 02:18:20 +0700

    Updates

---
+++ precache-manifest.a05b496c31ec44f0a5c829348840843b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028fed443551f324b47d9f856c473183",
+    "url": "/index.html"
+  },
+  {
+    "revision": "855e31eaa37dc2c2626c",
+    "url": "/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "6533ed4825b89cf3d08c",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "855e31eaa37dc2c2626c",
+    "url": "/static/js/2.c57e7251.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c57e7251.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6533ed4825b89cf3d08c",
+    "url": "/static/js/main.da2866fe.chunk.js"
+  },
+  {
+    "revision": "03357149483a376b5b2e",
+    "url": "/static/js/runtime-main.b09effc8.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
